fae7cf936a58705badd372798e7dc02e396e3d60
YeoheungMinSsi <20161538@office.dongseo.ac.kr>
2025-12-03 21:11:00 +0900

    프론트 데이터(클라이언트 데이터)를 사용하지않고 서버에 저장된 데이터를 활용해서 돌아가도록 설정함

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 💡 [핵심] CORS 설정: Vite 개발 서버 (http://localhost:5173)의 요청을 허용합니다.
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // 서버 포트는 3000번을 사용합니다.
+  await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
